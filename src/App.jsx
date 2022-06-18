6e6b84da04598d53f47c1f01699efdc57826bfd0
Helena Oliveira <helena@informach.com.br>
2022-06-18 11:31:51 -0300

    add header and sidebar components

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { Post } from './Post';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Helena Oliveira"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae magnam ratione repellat, ipsum totam illo ab optio illum. Ducimus a ipsum sed iure rerum? Impedit, ducimus corporis. Dolor, enim?"
+          />
+          <Post
+            author="Júlio Felipe"
+            content="New Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae magnam ratione repellat, ipsum totam illo ab optio illum. Ducimus a ipsum sed iure rerum? Impedit, ducimus corporis. Dolor, enim?"
+          />
+        </main>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1587612049655-c1030366a74a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGRldmVsb3BtZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50" alt="" />
+            
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/oliveirahelena.png" alt="" />
+
+                <strong>Helena Oliveira</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
